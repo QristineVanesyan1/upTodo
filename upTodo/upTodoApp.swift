5df3cdbcab190d9923a65cde832ff7c324835b2b
christina.vanesyan@gmail.com <80003543+QristineVanesyan1@users.noreply.github.com>
2023-01-08 17:21:51 +0400

    init

---
+++ upTodo/upTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  upTodoApp.swift
+//  upTodo
+//
+//  Created by Christine Vanesyan on 1/8/23.
+//
+
+import SwiftUI
+
+@main
+struct upTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
